5062739aa33e9d85ca40436d70f89da97f26c650
JessieZY1123 <JessieZY1123@gmail.com>
2022-07-27 22:14:40 -0400

    create folders

---
+++ MovieShop.WebApp/MovieShop.Infrastructure/Repository/CastRepository.cs
@@ -0,0 +1,13 @@
+﻿using MovieShop.ApplicationCore.Contracts.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieShop.Infrastructure.Repository
+{
+    public class CastRepository: ICastRepository
+    {
+    }
+}
---
+++ MovieShop.WebApp/MovieShop.WebApp/Controllers/CastController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShop.WebApp.Controllers
+{
+    public class CastController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
